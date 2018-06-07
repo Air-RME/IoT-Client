d0a1a804f8654d300e8d5ebe2bc87fcb26df77e1
Kristoffer <kba_93@live.se>
2018-06-07 16:43:35 +0900

    サンプルechoスクリプト

--- samples/ShadowEcho.py
+++ samples/ShadowEcho.py
@@ -1,1 +1,27 @@
-from AWSIoTPythonSDK.MQTTLib from AWS
+from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTShadowClient
+import json
+import os
+
+
+def echoCallback(payload, responseStatus, token):
+    p = json.loads(payload)
+    print(p)
+
+
+# クライアントセットアップ
+shadowClient = AWSIoTMQTTShadowClient("")
+shadowClient.configureEndpoint("a1g1flllk3y7ps.iot.ap-northeast-1.amazonaws.com", 8883)
+shadowClient.configureCredentials("../credentials/aws/aws-root.pem", "../credentials/device/4bb00f45b1-private.pem.key",
+                                  "../credentials/device/4bb00f45b1-certificate.pem.crt")
+shadowClient.configureConnectDisconnectTimeout(10)
+shadowClient.configureMQTTOperationTimeout(5)
+
+shadowClient.connect()
+
+shadowDevice = shadowClient.createShadowHandlerWithName("Air-RME-test", True)
+
+shadowDevice.shadowGet(echoCallback, 5)
+shadowDevice.shadowRegisterDeltaCallback(echoCallback)
+shadowDevice.shadowUnregisterDeltaCallback()
+
+shadowClient.disconnect()
