eaf26ea5ac60519435929637091e1a78296235eb
Kristoffer <kba_93@live.se>
2018-07-19 13:58:59 +0900

    setup update

--- setup.sh
+++ setup.sh
@@ -8,6 +8,9 @@ cd ..
 git clone https://github.com/adafruit/Adafruit_Python_DHT.git
 cd Adafruit_Python_DHT
 
+sudo apt-get update
+sudo apt-get install build-essential python-dev
+
 sudo python setup.py install
 sudo python3 setup.py install
 
