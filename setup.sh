8f7f39cb611c34127d6eb476ab322c8bbf914ab8
Kristoffer <kba_93@live.se>
2018-07-19 13:25:14 +0900

    setup

---
+++ setup.sh
@@ -0,0 +1,15 @@
+git clone https://github.com/aws/aws-iot-device-sdk-python.git
+cd aws-iot-device-sdk-python
+
+sudo python3 setup.py install
+
+cd ..
+
+git clone https://github.com/adafruit/Adafruit_Python_DHT.git
+cd Adafruit_Python_DHT
+
+sudo python setup.py install
+sudo python3 setup.py install
+
+rm -rf aws-iot-device-sdk-python
+rm -rf Adafruit_Python_DHT
